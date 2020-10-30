41841790cbd8f371a4a704650b633219cc5f2369
Asus <Asus@LAPTOP-VA2LRPBV>
2020-10-30 23:13:04 +0530

    //Change Xpath

--- AssignSolution1/src/test/java/testcases/Login.java
+++ AssignSolution1/src/test/java/testcases/Login.java
@@ -1,40 +1,41 @@
 package testcases;
 
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Login {
 
-	public static void main(String[] args) throws InterruptedException 
+	public static void main(String[] args) 
 	{
-		//Launching the Web Page
-			System.setProperty("webdriver.chrome.driver", "Z:\\Software\\Selenium SW\\chromedriver85.exe");
-			WebDriver driver = new ChromeDriver();
-			driver.get("http://new.koode.in/#/login/0");
-			driver.manage().window().maximize();
-			System.out.println("Page Title is :-" +driver.getTitle());
-			System.out.println("Page URL is :-" +driver.getCurrentUrl());
-			Thread.sleep(2000);
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://new.koode.in/#/login/0");
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 				
-		//Login Process
-			//Enter Email id
-			driver.findElement(By.xpath("//*[@id=\"app\"]/div/section/div[2]/div/div/div[2]/div[1]/form/fieldset/div[1]/input")).sendKeys("mahi@gmail.com");
-			Thread.sleep(2000);
-			//Enter the Password
-			driver.findElement(By.xpath("//*[@id=\"app\"]/div/section/div[2]/div/div/div[2]/div[1]/form/fieldset/div[2]/input")).sendKeys("Abcd123");
-			Thread.sleep(2000);
-			//Click the Checkbox
-			driver.findElement(By.xpath("//*[@id=\"app\"]/div/section/div[2]/div/div/div[2]/div[1]/form/fieldset/div[3]/label/input")).click();
-			Thread.sleep(2000);
-				
-			//Click on Sing in button
-			driver.findElement(By.xpath("//*[@id=\"app\"]/div/section/div[2]/div/div/div[2]/div[1]/form/fieldset/div[4]/button")).click();
-			Thread.sleep(5000);
+		//Enter Email id
+		driver.findElement(By.xpath("//div[@class='form-login uk-width-2-5@m uk-first-column']//input[@placeholder='Email']")).sendKeys("mahi@gmail.com");
 			
-			//Close the browser
-			driver.close();
+		//Enter the Password
+		driver.findElement(By.xpath("//div[@class='form-login uk-width-2-5@m uk-first-column']//input[@placeholder='Password']")).sendKeys("Abcd123");
+			
+		//Click the Checkbox
+		driver.findElement(By.xpath("//input[@type='checkbox']")).click();
+			
+		//Click on Sing in button
+		driver.findElement(By.xpath("//div[@class='uk-margin']//button[@type='submit'][contains(normalize-space(),'Sign In')]")).click();
+			
+		//Close the browser
+		driver.close();
 				
 	}
 
